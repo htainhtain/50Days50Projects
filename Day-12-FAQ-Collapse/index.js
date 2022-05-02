26b26ca8991c8f73db07a5f7c405987d94d8256e
htainhtain <htainlynnaung1996@gmail.com>
2022-05-02 22:23:06 +0700

    finish day 12

---
+++ Day-12-FAQ-Collapse/index.js
@@ -0,0 +1,17 @@
+let faqBoxes = document.querySelectorAll('.faq-box')
+
+
+
+faqBoxes.forEach(faqBox => {
+    faqBox.addEventListener('click', event => {
+        if (event.target.classList.contains('fa-chevron-down')) {
+            event.target.classList.replace('fa-chevron-down', 'fa-circle-xmark')
+            faqBox.classList.add('open')
+            faqBox.lastElementChild.classList.add('open')
+        } else if(event.target.classList.contains('fa-circle-xmark')){
+            event.target.classList.replace('fa-circle-xmark', 'fa-chevron-down')
+            faqBox.classList.remove('open')
+            faqBox.lastElementChild.classList.remove('open')
+        }
+    })
+})
