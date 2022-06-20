dbc779d3a91265820efc6c978ddea11c2cc4fe2b
htainhtain <htainlynnaung1996@gmail.com>
2022-06-20 20:03:48 +0700

    finally 21 finished

---
+++ Day-21-Drag-and-Drop/index.js
@@ -0,0 +1,86 @@
+let boxesDom = document.querySelectorAll(".box");
+
+const dragElement = (elmnt) => {
+  let pos1 = 0,
+    pos2 = 0,
+    pos3 = 0,
+    pos4 = 0;
+  
+  let divWithCurrentImage;
+  let backgroundImageElemt = document.createElement("div");
+  backgroundImageElemt.classList.add("background-image");
+
+  const dragMouseDown = (e) => {
+    e = e || window.event;
+    e.preventDefault();
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    divWithCurrentImage = document.querySelector(".image");
+    if (e.target.classList.contains("image")) {
+      document.onmouseup = closeDragElement;
+      document.onmousemove = elementDrag;
+    }
+  };
+
+  const closeDragElement = (e) => {
+    document.onmouseup = null;
+    document.onmousemove = null;
+    backgroundImageElemt.remove();
+    checkBoxes2(e.clientX, e.clientY, "image");
+    if (!document.querySelector(".image")) { 
+      divWithCurrentImage.classList.add('image')
+    }
+    document.body.style.cursor = "default";
+  };
+
+  const elementDrag = (e) => {
+    e = e || window.event;
+    e.preventDefault();
+    document.body.appendChild(backgroundImageElemt);
+    elmnt.classList.remove("image");
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    checkBoxes1(e.clientX, e.clientY, "mouseover");
+    backgroundImageElemt.style.top = elmnt.offsetTop - pos2 + "px";
+    backgroundImageElemt.style.left = elmnt.offsetLeft - pos1 + "px";
+  };
+
+  const checkBoxes1 = (x, y, className) => {
+    for (let i = 0; i < boxesDom.length; i++) { 
+      boxesDom[i].classList.remove("mouseover");
+      document.body.style.cursor = "not-allowed";
+      if (
+        x > boxesDom[i].offsetLeft &&
+        x < boxesDom[i].offsetLeft + boxesDom[i].offsetWidth &&
+        y > boxesDom[i].offsetTop &&
+        y < boxesDom[i].offsetTop + boxesDom[i].offsetHeight
+      ) {
+        document.body.style.cursor = "pointer";
+        boxesDom[i].classList.add(className);
+        return;
+      }
+    }
+  };
+
+  const checkBoxes2 = (x, y, className) => {
+    for (let i = 0; i < boxesDom.length; i++) { 
+      boxesDom[i].classList.remove("mouseover");
+      if (
+        x > boxesDom[i].offsetLeft &&
+        x < boxesDom[i].offsetLeft + boxesDom[i].offsetWidth &&
+        y > boxesDom[i].offsetTop &&
+        y < boxesDom[i].offsetTop + boxesDom[i].offsetHeight
+      ) {
+        boxesDom[i].classList.add(className);
+      } else {
+        boxesDom[i].classList.remove(className);
+      }
+    }
+  };
+
+  elmnt.onmousedown = dragMouseDown;
+};
+
+boxesDom.forEach((box) => {
+  dragElement(box);
+});
