812f675fc6c9dc0e2df1a43fef75bfd6b796d8be
htainhtain <htainlynnaung1996@gmail.com>
2022-06-29 20:23:07 +0700

    day 24 refactor

--- day-24-contentplaceholder/index.js
+++ day-24-contentplaceholder/index.js
@@ -1,49 +1,21 @@
 const imagesDom = document.querySelectorAll("img");
-const contentCardDescriptionTextLoader = document.querySelectorAll(
-  ".content-card-description-text-loader"
-);
-const avatarDescriptionLoader = document.querySelectorAll(
-  ".avatar-description-loader"
-);
-
-const contentImgLoader = document.querySelector(".content-card-img-loader");
-const avatarImgLoader = document.querySelector(".avatar-img-loader");
-const h3 = document.querySelector("h3");
-const p = document.querySelector("p");
-const h5 = document.querySelector("h5");
-const span = document.querySelector("span");
-
-const fullyLoaded = false;
+const hiddenContentDoms = document.querySelectorAll(".hidden-content");
+const loaderDoms = document.querySelectorAll(".loader");
 let counter = 0;
-
-h3.style.visibility = "hidden";
-p.style.visibility = "hidden";
-h5.style.visibility = "hidden";
-span.style.visibility = "hidden";
 
 const checkAllLoaded = () => {
   counter++;
   if (counter === imagesDom.length) {
-    contentImgLoader.classList.remove("load");
-    avatarImgLoader.classList.remove("load");
-    contentCardDescriptionTextLoader.forEach((loader) => {
-      loader.classList.remove("load");
-      h3.style.visibility = "visible";
-      p.style.visibility = "visible";
+    hiddenContentDoms.forEach((hiddenContentDom) => {
+      hiddenContentDom.classList.remove("hidden-content");
     });
-    imagesDom.forEach((imageDom) => {
-      imageDom.style.visibility = "visible";
-    });
-    avatarDescriptionLoader.forEach((avatar) => {
-      avatar.classList.remove("load");
-      h5.style.visibility = "visible";
-      span.style.visibility = "visible";
+    loaderDoms.forEach((loaderDom) => {
+      loaderDom.classList.remove("load");
     });
   }
 };
 
 imagesDom.forEach((imageDom) => {
-  imageDom.style.visibility = "hidden";
   imageDom.addEventListener("load", () => {
     checkAllLoaded();
   });
